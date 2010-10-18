d65c225d599c32dc4c84565b4780aaafa6c36b13
Tim Martin <tim@asymptotic.co.uk>
2010-10-18 20:52:58 +0200

    Updated the unit tests for v2 API compatibility

--- tests/pChartTest.php
+++ tests/pChartTest.php
@@ -100,7 +100,7 @@ class pChartTest extends PHPUnit_Framework_TestCase {
 		$DataSet->AddPoint(array(10, 2, 3, 5, 3), "Serie1");
 		$DataSet->AddPoint(array("Jan", "Feb", "Mar", "Apr", "May"), "Serie2");
 		$DataSet->AddAllSeries();
-		$DataSet->SetAbsciseLabelSerie("Serie2");
+		$DataSet->SetAbscissaLabelSeries("Serie2");
 		
 		// Initialise the graph
 		$Test = new pChart(300, 200);
@@ -183,9 +183,9 @@ class pChartTest extends PHPUnit_Framework_TestCase {
 		$DataSet->AddPoint(array(1,4,3,2,3,3,2,1,0,7,4,3,2,3,3,5,1,0,7),"Serie1");
 		$DataSet->AddPoint(array(1,4,2,6,2,3,0,1,5,1,2,4,5,2,1,0,6,4,2),"Serie2");
 		$DataSet->AddAllSeries();
-		$DataSet->SetAbsciseLabelSerie();
-		$DataSet->SetSerieName("January","Serie1");
-		$DataSet->SetSerieName("February","Serie2");
+		$DataSet->SetAbscissaLabelSeries();
+		$DataSet->SetSeriesName("January","Serie1");
+		$DataSet->SetSeriesName("February","Serie2");
 		
 		// Initialise the graph
 		$Test = new pChart(700,230);
