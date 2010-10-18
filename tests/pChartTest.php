68fc2e928e82f00a2673b64ac95f7f04daed5b34
Tim Martin <tim@asymptotic.co.uk>
2010-10-18 20:39:56 +0200

    Unit test coverage for drawFlatPieGraph

--- tests/pChartTest.php
+++ tests/pChartTest.php
@@ -94,6 +94,36 @@ class pChartTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals($expectedContents, $actualContents);
 	}
 
+	public function testFlatPieGraph() {
+		// Dataset definition 
+		$DataSet = new pData();
+		$DataSet->AddPoint(array(10, 2, 3, 5, 3), "Serie1");
+		$DataSet->AddPoint(array("Jan", "Feb", "Mar", "Apr", "May"), "Serie2");
+		$DataSet->AddAllSeries();
+		$DataSet->SetAbsciseLabelSerie("Serie2");
+		
+		// Initialise the graph
+		$Test = new pChart(300, 200);
+		$Test->setFontProperties(dirname(__FILE__)."/../Fonts/tahoma.ttf", 8);
+		$Test->drawFilledRoundedRectangle(7, 7, 293, 193, 5, 240, 240, 240);
+		$Test->drawRoundedRectangle(5, 5, 295, 195, 5, 230, 230, 230);
+		
+		// Draw the pie chart
+		$Test->setAntialiasQuality(0);
+		$Test->setShadowProperties(2, 2, 200, 200, 200);
+		$Test->drawFlatPieGraphWithShadow($DataSet->GetData(), 
+										  $DataSet->GetDataDescription(), 120, 100, 60, PIE_PERCENTAGE, 8);
+		$Test->clearShadow();
+		$Test->drawPieLegend(230, 15, $DataSet->GetData(), 
+							 $DataSet->GetDataDescription(), 250, 250, 250);
+		$Test->Render(dirname(__FILE__)."/actual/example13.png");
+
+		$expectedContents = file_get_contents(dirname(__FILE__).'/expected/example13.png');
+		$actualContents = file_get_contents(dirname(__FILE__).'/actual/example13.png');
+		
+		$this->assertEquals($expectedContents, $actualContents);
+	}
+
 	/**
 	 * Based on Example24.php
 	 */
