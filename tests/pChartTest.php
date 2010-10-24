ec2d4dae9d6e70e0b2038c27dc43cc2e95623e38
Tim Martin <tim@asymptotic.co.uk>
2010-10-24 15:04:44 +0200

    Unit testing

--- tests/pChartTest.php
+++ tests/pChartTest.php
@@ -321,8 +321,55 @@ class pChartTest extends PHPUnit_Framework_TestCase {
 
 		$chart->render(dirname(__FILE__).'/actual/xyplot.png');
 
-				$expectedContents = file_get_contents(dirname(__FILE__).'/expected/xyplot.png');
+		$expectedContents = file_get_contents(dirname(__FILE__).'/expected/xyplot.png');
 		$actualContents = file_get_contents(dirname(__FILE__).'/actual/xyplot.png');
+
+		$this->assertTrue($expectedContents == $actualContents);
+	}
+
+	public function testLabels() {
+		// Dataset definition 
+		$DataSet = new pData;
+		$DataSet->AddPoint(array(0,70,70,0,0,70,70,0,0,70),"Serie1");
+		$DataSet->AddPoint(array(0.5,2,4.5,8,12.5,18,24.5,32,40.5,50),"Serie2");
+
+		$DataSet->AddAllSeries();
+		$DataSet->SetAbsciseLabelSerie();
+		$DataSet->SetSerieName("January","Serie1");
+		$DataSet->SetSerieName("February","Serie2");
+
+		// Initialise the graph
+		$Test = new pChart(700,230);
+		$Test->setFontProperties(dirname(__FILE__)."/../Fonts/tahoma.ttf",8);
+		$Test->setGraphArea(50,30,585,200);
+		$Test->drawFilledRoundedRectangle(7,7,693,223,5,240,240,240);
+		$Test->drawRoundedRectangle(5,5,695,225,5,230,230,230);
+		$Test->drawGraphArea(255,255,255,TRUE);
+		$Test->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,TRUE,0,2);
+		$Test->drawGrid(4,TRUE,230,230,230,50);
+
+		// Draw the 0 line
+		$Test->setFontProperties(dirname(__FILE__)."/../Fonts/tahoma.ttf",6);
+		$Test->drawTreshold(0,143,55,72,TRUE,TRUE);
+
+		// Draw the line graph
+		$Test->drawLineGraph($DataSet->GetData(),$DataSet->GetDataDescription());
+		$Test->drawPlotGraph($DataSet->GetData(),$DataSet->GetDataDescription(),3,2,255,255,255);
+
+		// Set labels
+		$Test->setFontProperties(dirname(__FILE__)."/../Fonts/tahoma.ttf",8);
+		$Test->setLabel($DataSet->GetData(),$DataSet->GetDataDescription(),"Serie1","2","Daily incomes",221,230,174);
+		$Test->setLabel($DataSet->GetData(),$DataSet->GetDataDescription(),"Serie2","6","Production break",239,233,195);
+
+		// Finish the graph
+		$Test->setFontProperties(dirname(__FILE__)."/../Fonts/tahoma.ttf",8);
+		$Test->drawLegend(600,30,$DataSet->GetDataDescription(),255,255,255);
+		$Test->setFontProperties(dirname(__FILE__)."/../Fonts/tahoma.ttf",10);
+		$Test->drawTitle(50,22,"Example 9",50,50,50,585);
+		$Test->Render(dirname(__FILE__)."/actual/example9.png");
+
+		$expectedContents = file_get_contents(dirname(__FILE__).'/expected/example9.png');
+		$actualContents = file_get_contents(dirname(__FILE__).'/actual/example9.png');
 
 		$this->assertTrue($expectedContents == $actualContents);
 	}
