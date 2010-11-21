20b74922605bbe56b53a53ace2b8bcd4711bc085
Tim Martin <tim@asymptotic.co.uk>
2010-11-21 09:01:36 +0200

    The TestCanvas can now cope with arrays being passed as method arguments

--- lib/TestCanvas.php
+++ lib/TestCanvas.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).'/ICanvas.php';
 
 class TestCanvas implements ICanvas {
 	function drawRectangle(Point $corner1, Point $corner2, Color $color, $lineWidth, $lineDotSize, ShadowProperties $shadowProperties) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawFilledRectangle(Point $corner1, Point $corner2, Color $color,
@@ -13,35 +13,35 @@ class TestCanvas implements ICanvas {
 								 $alpha = 100,
 								 $lineWidth = 1,
 								 $lineDotSize = 0) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawRoundedRectangle(Point $corner1, Point $corner2, $radius,
 								  Color $color, $lineWidth, $lineDotSize,
 								  ShadowProperties $shadowProperties) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawFilledRoundedRectangle(Point $point1, Point $point2, $radius,
 										Color $color, $lineWidth, $lineDotSize,
 										ShadowProperties $shadowProperties) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawLine(Point $point1, Point $point2, Color $color, $lineWidth, $lineDotSize, ShadowProperties $shadowProperties, Point $boundingBoxMin = null, Point $boundingBoxMax = null) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawDottedLine(Point $point1, Point $point2, $dotSize, $lineWidth, Color $color, ShadowProperties $shadowProperties, Point $boundingBoxMin = null, Point $boundingBoxMax = null) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawAntialiasPixel(Point $point, Color $color, ShadowProperties $shadowProperties, $alpha) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawText($fontSize, $angle, Point $point, Color $color, $fontName, $text, ShadowProperties $shadowProperties) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	/**
@@ -50,19 +50,32 @@ class TestCanvas implements ICanvas {
 	 * height. This should be changed.
 	 */
 	function drawCircle(Point $center, $height, Color $color, ShadowProperties $shadowProperties, $width) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawFilledCircle(Point $center, $height, Color $color, ShadowProperties $shadowProperties, $width = null) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function drawFilledPolygon(array $points, $numPoints, Color $color) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
 	}
 
 	function setAntialiasQuality($newQuality) {
-		$this->actionLog .= __METHOD__.'('.implode(', ', func_get_args()).")\n";
+		$this->logMethodCall(__METHOD__, func_get_args());
+	}
+
+	private function logMethodCall($methodName, $args) {
+		$formattedArgs = array();
+		foreach ($args as $arg) {
+			if (is_array($arg)) {
+				$formattedArgs[] = 'array<' . implode(', ', $arg) . '>';
+			}
+			else {
+				$formattedArgs[] = $arg;
+			}
+		}
+		$this->actionLog .= $methodName.'('.implode(', ', $formattedArgs).")\n";
 	}
 
 	public function getActionLog() {
