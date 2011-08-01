e64111b528223e66f33b8a668670ca075ada5ce3
sebix <szebi@gmx.at>
2011-08-01 20:17:05 +0200

    Fixed Example 5

--- examples/Example5.php
+++ examples/Example5.php
@@ -1,42 +1,47 @@
 <?php
- /*
-     Example5 : A limits graph
+
+/*
+  Example5 : A limits graph
  */
 
- // Standard inclusions   
- require_once("../lib/pData.php");
- require_once("../lib/pChart.php");
+// Standard inclusions   
+require_once("../lib/pData.php");
+require_once("../lib/pChart.php");
+require_once '../lib/GDCanvas.php';
+require_once '../lib/BackgroundStyle.php';
 
- // Dataset definition 
- $DataSet = new pData;
- $DataSet->AddPoint(array(1,4,-3,2,-3,3,2,1,0,7,4,-3,2,-3,3,5,1,0,7),"Serie1");
- $DataSet->AddPoint(array(2,5,7,5,1,5,6,4,8,4,0,2,5,6,4,5,6,7,6),"Serie2");
- $DataSet->AddAllSeries();
- $DataSet->SetAbsciseLabelSerie();
- $DataSet->SetSerieName("January","Serie1");
- $DataSet->SetSerieName("February","Serie2");
+// Dataset definition 
+$DataSet = new pData;
+$DataSet->addPoints(array(1, 4, -3, 2, -3, 3, 2, 1, 0, 7, 4, -3, 2, -3, 3, 5, 1, 0, 7), "Serie1");
+$DataSet->addPoints(array(2, 5, 7, 5, 1, 5, 6, 4, 8, 4, 0, 2, 5, 6, 4, 5, 6, 7, 6), "Serie2");
+$DataSet->AddAllSeries();
+$DataSet->SetAbscissaLabelSeries();
+$DataSet->SetSeriesName("January", "Serie1");
+$DataSet->SetSeriesName("February", "Serie2");
 
- // Initialise the graph
- $Test = new pChart(700,230);
- $Test->setFontProperties("../Fonts/tahoma.ttf",8);
- $Test->setGraphArea(50,30,585,200);
- $Test->drawFilledRoundedRectangle(7,7,693,223,5,240,240,240);
- $Test->drawRoundedRectangle(5,5,695,225,5,230,230,230);
- $Test->drawGraphArea(255,255,255,TRUE);
- $Test->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,TRUE,0,2,TRUE);
- $Test->drawGrid(4,TRUE,230,230,230,50);
+// Initialise the graph
+$canvas = new GDCanvas(700, 230);
+$Test = new pChart(700, 230, $canvas);
+$Test->setFontProperties("../Fonts/tahoma.ttf", 8);
+$Test->setGraphArea(50, 30, 585, 200);
+$canvas->drawFilledRoundedRectangle(new Point(7, 7), new Point(693, 223), 5, new Color(240, 240, 240), 1, 0, ShadowProperties::NoShadow());
+$backgroundStyle = new BackgroundStyle(new Color(255, 255, 255), TRUE);
+$Test->drawGraphBackground($backgroundStyle);
 
- // Draw the 0 line
- $Test->setFontProperties("../Fonts/tahoma.ttf",6);
- $Test->drawTreshold(0,143,55,72,TRUE,TRUE);
+$Test->drawScale($DataSet, ScaleStyle::DefaultStyle(), 0, 2, TRUE);
+$Test->drawGrid(new GridStyle(4, TRUE, new Color(230, 230, 230), 50));
 
- // Draw the limit graph
- $Test->drawLimitsGraph($DataSet->GetData(),$DataSet->GetDataDescription(),180,180,180);
+// Draw the 0 line
+$Test->setFontProperties("../Fonts/tahoma.ttf", 6);
+$Test->drawTreshold(0, new Color(143, 55, 72), TRUE, TRUE);
 
- // Finish the graph
- $Test->setFontProperties("../Fonts/tahoma.ttf",8);
- $Test->drawLegend(600,30,$DataSet->GetDataDescription(),255,255,255);
- $Test->setFontProperties("../Fonts/tahoma.ttf",10);
- $Test->drawTitle(50,22,"Example 5",50,50,50,585);
- $Test->Render("Example5.png");
+// Draw the limit graph
+$Test->drawLimitsGraph($DataSet->GetData(), $DataSet->GetDataDescription(), new Color(180, 180, 180));
+
+// Finish the graph
+$Test->setFontProperties("../Fonts/tahoma.ttf", 8);
+$Test->drawLegend(600, 30, $DataSet->GetDataDescription(), new Color(255, 255, 255));
+$Test->setFontProperties("../Fonts/tahoma.ttf", 10);
+$Test->drawTitle(50, 22, "Example 5", new Color(50, 50, 50), 585);
+$Test->Render("Example5.png");
 ?>
