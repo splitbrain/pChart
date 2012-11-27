08de3518d46bd354e589b59c3e4841196104e89c
Andreas Gohr <andi@splitbrain.org>
2012-11-27 22:26:13 +0100

    added HTML output to buildAll.sh

--- examples/buildAll.sh
+++ examples/buildAll.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+echo '<html><body><h1>pChart Examples</h1>' > index.html
+
+
 for i in `ls -v *.php`; do
-   NAME=`echo $i | sed 's/.php//'`
-   echo -n $NAME:\ 
-   head -n 1 ${NAME}.txt
-   php -q ${NAME}.php > /dev/null 2>&1
-   if [ $? -ne 0 ]; then
-      echo -e "\033[31mError!\033[0m" # red
-   else
-      echo -e "\033[32mPassed!\033[0m" # green
-   fi
+    NAME=`echo $i | sed 's/.php//'`
+    TITLE=`head -n 1 ${NAME}.txt`
+    echo $NAME: $TITLE
+    echo "<h2>$NAME: $TITLE</h2>" >> index.html
+
+    php -q ${NAME}.php > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo -e "\033[31mError!\033[0m" # red
+        echo '<p>Failed</p>' >> index.html
+    else
+        echo -e "\033[32mPassed!\033[0m" # green
+        echo "<img src=\"$NAME.png\">" >> index.html
+    fi
 done;
+
+echo '</body></html>' >> index.html
